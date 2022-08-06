e61d036307fa3625f5064b25f175ed2bb24ec493
fahadhossien72 <63352657+fahadhossien72@users.noreply.github.com>
2022-08-06 17:39:28 +0600

    Update settings.py

--- crm2/settings.py
+++ crm2/settings.py
@@ -137,4 +137,4 @@ EMAIL_HOST = 'smtp@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sifadrifad61@gmail.com'
-EMAIL_HOST_PASSWORD = 'sifadrifadfahad'
+EMAIL_HOST_PASSWORD = ''
